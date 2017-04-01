1000481f08f57ed3d8d76c0564496f853b81f40e
Eddie-Joe Robinson <Eddie@Eddie-Robinsons-MacBook-Pro1.local>
2017-04-01 19:25:20 +0100

    still trying to get it to initially work

--- pappyJS.js
+++ pappyJS.js
@@ -211,7 +211,6 @@ if ($('#BGimage').length > 0) {
 });
 	} else {
 	$("#header").show();
-	$("#menu").show();
 	$("#blackStrip").show();
 	$("#logo").delay(300).show();
 	$(".headerOptionAbout").hide();
