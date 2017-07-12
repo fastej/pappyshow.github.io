3ad53431385a6643651428c34060707bbdb58e38
Eddie-Joe Robinson <Eddie@Eddie-Robinsons-MacBook-Pro1.local>
2017-07-12 23:54:32 +0100

    trying to fix booking

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = array(
-  "secret_key"      => "sk_test_FIGv7KGDcVruL4PMvqQyWgkA",
-  "publishable_key" => "pk_test_8dF8LMXDYK7FMSomqQTutxKo"
+  "secret_key"      => "sk_live_VhEksKO55ic2sPmHQrnaG6Cc",
+  "publishable_key" => "pk_live_itopHgfvk2VnqXOijZ68CzVm"
 );
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
